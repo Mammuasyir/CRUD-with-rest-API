1e6b350d4cf072153be28faedfdfac8b65f97571
emRival <em.rival282@gmail.com>
2022-06-19 14:25:18 +0700

    delete

--- app/src/main/java/com/rival/my_packet/adapter/paket/PaketAdapter.kt
+++ app/src/main/java/com/rival/my_packet/adapter/paket/PaketAdapter.kt
@@ -1,24 +1,29 @@
 package com.rival.my_packet.adapter.paket
 
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.rival.my_packet.R
+import com.rival.my_packet.api.ApiConfig
 import com.rival.my_packet.helper.SharedPreference
+import com.rival.my_packet.model.ResponsePaket
 import com.rival.my_packet.model.ResultItem
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class PaketAdapter(var paket: List<ResultItem?>? = listOf()) : RecyclerView.Adapter<PaketAdapter.MyViewHolder>()  {
-    lateinit var sph : SharedPreference
-    class MyViewHolder (val view:View) : RecyclerView.ViewHolder(view) {
+class PaketAdapter(var paket: List<ResultItem?>? = listOf()) :
+    RecyclerView.Adapter<PaketAdapter.MyViewHolder>() {
+    lateinit var sph: SharedPreference
+
+    class MyViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
 
         val tvTitle = view.findViewById<TextView>(R.id.txt_title)
         val btnDetail = view.findViewById<ImageButton>(R.id.btn_detail)
@@ -33,7 +38,10 @@ class PaketAdapter(var paket: List<ResultItem?>? = listOf()) : RecyclerView.Adap
         return MyViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+    override fun onBindViewHolder(
+        holder: MyViewHolder,
+        @SuppressLint("RecyclerView") position: Int
+    ) {
         val context = holder.itemView.context
         val data = paket?.get(position)
         holder.tvTitle.text = data?.nama_penerima
@@ -47,7 +55,41 @@ class PaketAdapter(var paket: List<ResultItem?>? = listOf()) : RecyclerView.Adap
 //            holder.btnDelete.visibility = View.GONE
 //        }
 
+        holder.btnDelete.setOnClickListener {
+            val builder = AlertDialog.Builder(context)
+            builder.setTitle("Hapus")
+            builder.setMessage("Apakakah yakin hapus paket ${data?.nama_penerima} ?")
+            builder.setPositiveButton("Ya") { dialog, which ->
+                ApiConfig.instanceRetrofit.deletePaket(data?.id!!).enqueue(object :
+                    Callback<ResponsePaket> {
+                    override fun onResponse(
+                        call: Call<ResponsePaket>,
+                        response: Response<ResponsePaket>
+                    ) {
+                        if (response.isSuccessful) {
+                            if (response.body()?.status == 1) {
+                                Toast.makeText(context, "Berhasil hapus paket", Toast.LENGTH_SHORT)
+                                    .show()
+                                paket?.get(position)
+                                notifyItemRemoved(paket?.indexOf(data)!!)
+                                itemCount.minus(-1)
+                            }
+                        } else {
+                            Toast.makeText(context, "Gagal hapus paket", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    }
 
+                    override fun onFailure(call: Call<ResponsePaket>, t: Throwable) {
+                        Toast.makeText(context, "Gagal hapus paket", Toast.LENGTH_SHORT).show()
+                    }
+                })
+            }
+            builder.setNegativeButton("Tidak") { dialog, which ->
+                dialog.dismiss()
+            }
+            builder.show()
+        }
 
 
 
@@ -63,7 +105,8 @@ class PaketAdapter(var paket: List<ResultItem?>? = listOf()) : RecyclerView.Adap
             val status = views.findViewById<TextView>(R.id.tv_status_paket)
             val UrlImage = "https://paket.siyap.co.id/storage/${data?.img}"
 
-            Glide.with(context).load(UrlImage).into(views.findViewById<ImageView>(R.id.img_paket_dtl))
+            Glide.with(context).load(UrlImage)
+                .into(views.findViewById<ImageView>(R.id.img_paket_dtl))
             nama.text = data?.nama_penerima
             tanggal.text = data?.tanggal_input
             status.text = data?.status
